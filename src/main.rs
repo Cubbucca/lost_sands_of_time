9d4d822381ba05136d05ae219213a8e796067840
Timothy Morton <wookie-hunter@hotmail.com>
2023-02-28 16:20:38 +1300

    floorbox

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+use std::f32::consts::PI;
+
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+pub const BACKGROUNDCOLOR: Color = Color::rgb(0.2, 0.2, 0.2);
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(BACKGROUNDCOLOR))
+        .add_startup_system(spawn_basic_scene)
+        .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: WIDTH,
+                height: HEIGHT,
+                title: "Lost Sands Of Time".to_string(),
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(asset_loading)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..default()
+        })
+        .insert(Name::new("Floor"));
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.67, 0.5, 0.1).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(Name::new("Box"));
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                intensity: 1500.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..default()
+        })
+        .insert(Name::new("PointLight"));
+}
+
+fn asset_loading(mut commands: Commands, assets: Res<AssetServer>) {
+    let scene0 = assets.load("pocket.glb#Scene0");
+    commands.spawn(SceneBundle {
+        scene: scene0,
+        transform: Transform::from_xyz(0.0, 2.0, 0.0).with_rotation(Quat::from_rotation_x(90.0)),
+        ..Default::default()
+    });
+}
